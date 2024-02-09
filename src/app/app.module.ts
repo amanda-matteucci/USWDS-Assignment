1615f557fcf120546a2f3172eac447e5a1841ba4
Amanda Matteucci <amatteucci1116@gmail.com>
2024-02-09 07:14:57 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+import { AboutModule } from './about/about.module';
+import { HomeModule } from './home/home.module';
+import { InfoModule } from './information/info.module';
+
+
+// Services
+import { ConfigService } from './services/config.service';
+import { InMemoryDataService } from './services/in-memory-data.service';
+import { TempFormModule } from './template-form/temp-form.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavigationComponent,
+		NavmenuComponent,
+	],
+	imports: [
+		AboutModule,
+		InfoModule,
+		AppRoutingModule,
+		BrowserModule,
+		HomeModule,
+		TempFormModule,
+		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+			dataEncapsulation: false,
+			passThruUnknownUrl: true,
+		})
+		
+	],
+	providers: [ConfigService],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
